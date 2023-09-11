83e6de96139a6364c880fa64353524814379e886
bawahirfebri26 <bawahirfebri@apps.ipb.ac.id>
2023-09-12 00:27:06 +0700

    Added the Flask project

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('Hello World!')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hallo World'
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)

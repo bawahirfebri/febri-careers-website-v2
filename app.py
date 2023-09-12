74524a4a6d3e780e12c824b29a2891fc2368e5f7
bawahirfebri26 <bawahirfebri@apps.ipb.ac.id>
2023-09-12 22:19:44 +0700

    dynamic data using templates

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Cilegon',
+        'salary': 'Rp. 3.000.000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Serang',
+        'salary': 'Rp. 4.000.000'
+    },
+    {
+        'id': 1,
+        'title': 'Frontend Engineer',
+        'location': 'Remote'
+    },
+    {
+        'id': 1,
+        'title': 'Backend Engineer',
+        'location': 'Bogor',
+        'salary': 'Rp. 5.000.000'
+    }
+]
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == '__main__':

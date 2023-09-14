cf473dc8356862ad4b5c3ace26b71717e1f5d3f6
bawahirfebri26 <bawahirfebri@apps.ipb.ac.id>
2023-09-15 04:12:28 +0700

    database connect

--- app.py
+++ app.py
@@ -1,40 +1,27 @@
 from flask import Flask, render_template, jsonify
+from database import engine
+from sqlalchemy import text
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Cilegon',
-        'salary': 'Rp. 3.000.000'
-    },
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Serang',
-        'salary': 'Rp. 4.000.000'
-    },
-    {
-        'id': 1,
-        'title': 'Frontend Engineer',
-        'location': 'Remote'
-    },
-    {
-        'id': 1,
-        'title': 'Backend Engineer',
-        'location': 'Bogor',
-        'salary': 'Rp. 5.000.000'
-    }
-]
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text('select * from jobs'))
+        jobs = []
+        for row in result.all():
+            jobs.append(row._mapping)
+
+    return jobs
 
 @app.route('/')
 def index():
-    return render_template('home.html', jobs=JOBS)
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs)
 
 @app.route('/api/jobs')
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db
+    return jsonify(jobs)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)

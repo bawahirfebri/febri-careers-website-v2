308377b59adc2c30dffe9ae73898ef0dd00d2f43
bawahirfebri26 <bawahirfebri@apps.ipb.ac.id>
2023-09-16 04:47:12 +0700

    submit form dan store to db

--- database.py
+++ database.py
@@ -13,16 +13,31 @@ engine = create_engine(
 
 def load_jobs_from_db():
     with engine.connect() as conn:
-        result = conn.execute(text('select * from jobs'))
+        result = conn.execute(text('SELECT * FROM jobs'))
         jobs = [row._asdict() for row in result.all()]
 
     return jobs
 
 def load_job_from_db(id):
     with engine.connect() as conn:
-        result = conn.execute(text('SELECT * FROM jobs WHERE id={}'.format(id)))
+        result = conn.execute(text('SELECT * FROM jobs WHERE id=:id'), {'id': id})
 
         job = [row._asdict() for row in result.all()]
         
         if len(job) == 0: return None
         else: return job[0]
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+
+        query = text('INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)')
+
+        conn.execute(query, {
+            'job_id': job_id,
+            'full_name': data['full_name'],
+            'email': data['email'],
+            'linkedin_url': data['linkedin_url'],
+            'education': data['education'],
+            'work_experience': data['work_experience'],
+            'resume_url': data['resume_url']
+        })
